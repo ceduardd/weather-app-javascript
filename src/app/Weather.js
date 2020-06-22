3f529da0145dfb5e10fd42164ba66285390a63fe
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-06-22 13:40:07 -0500

    formating code

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -1,21 +1,20 @@
 export class Weather {
+  constructor(city, countryCode) {
+    this.apikey = 'f75decd82fb40528b04e6693e39f6baf'
+    this.city = city
+    this.countryCode = countryCode
+  }
 
-    constructor(city, countryCode) {
-        this.apikey = 'f75decd82fb40528b04e6693e39f6baf'
-        this.city = city
-        this.countryCode = countryCode
-    }
+  async getWeather() {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`
 
-    async getWeather() {
-        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`
+    const response = await fetch(URL)
+    const data = await response.json()
+    return data
+  }
 
-        const response = await fetch(URL)
-        const data = await response.json()
-        return data
-    }
-
-    changeLocation(city, countryCode) {
-        this.city = city
-        this.countryCode = countryCode
-    }
+  changeLocation(city, countryCode) {
+    this.city = city
+    this.countryCode = countryCode
+  }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,25 +4,25 @@ const { Store } = require('./Store')
 require('./index.css')
 
 const ui = new UI()
-const store = new Store();
+const store = new Store()
 
 const { city, countryCode } = store.getLocationData()
 
 const weather = new Weather(city, countryCode)
 
 async function fetchWeather() {
-    const data = await weather.getWeather()
-    console.log(data)
-    ui.render(data)
+  const data = await weather.getWeather()
+  console.log(data)
+  ui.render(data)
 }
 
-document.getElementById('w-change-btn').addEventListener('click', e => {
-    const city = document.getElementById('city').value
-    const countryCode = document.getElementById('countryCode').value
-    weather.changeLocation(city, countryCode)
-    store.setLocationData(city, countryCode)
-    fetchWeather()
-    e.preventDefault()
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value
+  const countryCode = document.getElementById('countryCode').value
+  weather.changeLocation(city, countryCode)
+  store.setLocationData(city, countryCode)
+  fetchWeather()
+  e.preventDefault()
 })
 
 document.addEventListener('DOMContentLoaded', fetchWeather)
